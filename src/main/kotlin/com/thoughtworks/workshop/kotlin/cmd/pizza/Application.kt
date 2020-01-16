effbc67176d8172fdbd83bef12b5087ff07c665f
Rawat Pallavi <rpallavi@thoughtworks.com>
2020-01-16 19:36:56 +0530

    basic dsling

--- src/main/kotlin/com/thoughtworks/workshop/kotlin/cmd/pizza/Application.kt
+++ src/main/kotlin/com/thoughtworks/workshop/kotlin/cmd/pizza/Application.kt
@@ -1,5 +1,19 @@
 package com.thoughtworks.workshop.kotlin.cmd.pizza
 
+import com.thoughtworks.workshop.kotlin.pizza.FarmHouse
+import com.thoughtworks.workshop.kotlin.pizza.Pizza
+import com.thoughtworks.workshop.kotlin.pizza.Topping
+import com.thoughtworks.workshop.kotlin.pizza.description
+
+object CheeseBursts{
+    var pizza = FarmHouse()
+    infix fun farmHouse(pizza: Pizza): Pizza{
+        return pizza
+    }
+}
+
+var finalPizza: Pizza = FarmHouse()
+
 fun main() {
     /*
     val pizza: Pizza = CheeseBurst FarmHouse pizza {
@@ -8,4 +22,24 @@ fun main() {
 
     println(pizza.description())
     */
+
+    val topped: Pizza = FarmHouse()
+//    val pizza: Pizza = pizza {
+//        topped withh "Golden corn"
+//        topped withh "Pineapple"
+//    }
+//    println(pizza.description())
+
+    CheeseBursts farmHouse pizza {
+        topped withh "Golden Corn"
+
+    }
+}
+
+infix fun Pizza.withh(topping: String){
+    finalPizza = Topping(finalPizza, topping)
+}
+fun pizza(block: Pizza.() -> Unit): Pizza {
+    block(finalPizza)
+    return finalPizza
 }
